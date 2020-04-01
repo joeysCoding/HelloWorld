b9b6542a00231c7b0afed6f147d44bc9fd9ebc73
joeysCoding <35509374+joeysCoding@users.noreply.github.com>
2020-04-01 23:47:50 +0200

    Update Hello.java
    committed in GitHub

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("changed in intellij");
+        System.out.println("changed in intellij"); 
+        System.out.println("changed in GitHub");
     }
 }
